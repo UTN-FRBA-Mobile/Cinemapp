4b34dcf4a98d47d3e152407c64d9859093a06815
e-xifre <exifre@est.frba.utn.edu.ar>
2020-10-03 18:36:31 -0300

    funcionalidad del login - botón cancelar

--- app/src/main/java/com/utn/frba/cinemapp/Login.kt
+++ app/src/main/java/com/utn/frba/cinemapp/Login.kt
@@ -1,11 +1,25 @@
 package com.utn.frba.cinemapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //configura los botones
+        configButton();
+    }
+
+    private fun configButton(){
+        loginCancelButton.setOnClickListener {
+            val homeIntent = Intent(this, MainActivity::class.java).apply {
+            }
+            startActivity(homeIntent);
+        }
+
     }
 }
