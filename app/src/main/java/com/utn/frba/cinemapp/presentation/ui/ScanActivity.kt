c72e21f0af0de11973176b05b02b2525559316a6
Dante Castelluccio <dantecastelluccio@gmail.com>
2020-10-20 20:01:00 -0300

    add new view for QR scan

---
+++ app/src/main/java/com/utn/frba/cinemapp/presentation/ui/ScanActivity.kt
@@ -0,0 +1,13 @@
+package com.utn.frba.cinemapp.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.utn.frba.cinemapp.R
+
+class ScanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
+}
