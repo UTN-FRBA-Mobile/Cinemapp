004d936561a485e437107174dfe848a7dac7e35f
Dante Castelluccio <dantecastelluccio@gmail.com>
2020-11-26 01:40:23 -0300

    mocked data call to API real endpoints, should refactor

--- app/src/main/java/com/utn/frba/cinemapp/presentation/ui/ScanReadyActivity.kt
+++ app/src/main/java/com/utn/frba/cinemapp/presentation/ui/ScanReadyActivity.kt
@@ -7,9 +7,9 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.utn.frba.cinemapp.MainActivity
 import com.utn.frba.cinemapp.R
-import kotlinx.android.synthetic.main.activity_main.optionScan
 import kotlinx.android.synthetic.main.activity_scan_ready.*
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
 
 
@@ -22,30 +22,54 @@ class ScanReadyActivity : AppCompatActivity() {
         setContentView(R.layout.activity_scan_ready)
         title = findViewById(R.id.textView3)
         val b = intent
-        val code = b.getStringExtra("code")
+        var code = b.getStringExtra("code")
+        code = "bf142b24-a528-4ac6-a444-0986aa2dcc1a" //mock
         title!!.text = "Aguarde por favor..."
-
+        val userToken = "f68c6b39-f657-4ad9-928d-27dfbb26f4cc" //mock
         val client = OkHttpClient()
-        val url = "https://reqres.in/api/users?page=2"
+        val url =
+            "https://utn-2020-2c-desa-mobile.herokuapp.com/api/v1/discounts/byToken?token=$userToken"
+
+        val formBody: RequestBody = FormBody.Builder()
+            .add("id", code)
+            .build()
+        val JSON = MediaType.parse("application/json; charset=utf-8")
+        val data = JSONObject()
+        data.put("id", code.toString())
+        val body: RequestBody = RequestBody.create(JSON,data.toString())
         val request: Request = Request.Builder()
             .url(url)
+            .post(body)
             .build()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call?, e: IOException) {
+                Log.v("err", "ERRRR")
                 e.printStackTrace()
                 setupButtons()
             }
 
             override fun onResponse(call: Call?, response: Response) {
-                if (response.isSuccessful) {
+                Log.v("SUC", response.toString())
+                Log.v("SUCCC", response.body()!!.string())
+
+               /* if (response.isSuccessful) {
                     val myResponse: String = response.body()!!.string()
                     val mockedResponse = "20"
                     Log.v("response", myResponse)
+                    title!!.text = "Su descuento  de se ha acreditado con exito"
 
-                    saveDiscount(mockedResponse.toInt())
-                }
-                setupButtons()
+                    val url =
+                        "https://utn-2020-2c-desa-mobile.herokuapp.com/api/v1/discounts/byToken?token=$userToken"
+
+                    val request: Request = Request.Builder()
+                        .url(url)
+                        .get()
+                        .build()
+                    val response = client.newCall(request).execute()
+
+                    showDiscounts(response)
+                }*/
             }
         })
         setupButtons()
@@ -53,8 +77,8 @@ class ScanReadyActivity : AppCompatActivity() {
 
     }
 
-    private fun saveDiscount(discount: Int){
-        title!!.text = "Su descuento  de " + discount + "% se ha acreditado con exito"
+    private fun showDiscounts(response: Response){
+        Log.v("response", response.toString())
     }
 
     private fun setupButtons(){
