3ce6cc1dfd7648b8b332fbf029f919f16b2d1a3a
Brian <brian.d.lobo@gmail.com>
2020-11-29 15:24:51 -0300

    cambio de url para servicio de cinemas

--- app/src/main/java/com/utn/frba/cinemapp/interfaces/CinesApi.kt
+++ app/src/main/java/com/utn/frba/cinemapp/interfaces/CinesApi.kt
@@ -5,25 +5,31 @@ import com.utn.frba.cinemapp.models.login
 import com.utn.frba.cinemapp.models.register
 import com.utn.frba.cinemapp.models.seat
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface CinesApi {
-//    @GET("api/v1/cinemas/closest/{latitude}&{longitude}")
+    //    @GET("api/v1/cinemas/closest/{latitude}&{longitude}")
 //    https://utn-2020-2c-desa-mobile.herokuapp.com/api/v1/cinemas/closest?latitude=-34.604040&longitude=-58.411060
-    @GET("api/v1/cinemas/closest")
-    fun getCinesByProximity(@Query("latitude") latitude: String,
-                            @Query("longitude") longitude: String ) : Call<List<cine>>
+    @GET("api/v1/cinemas/closest/latitude/{latitude}/longitude/{longitude}")
+    fun getCinesByProximity(
+        @Path("latitude") latitude: String,
+        @Path("longitude") longitude: String
+    ): Call<List<cine>>
 
     //https://utn-2020-2c-desa-mobile.herokuapp.com/api/v1/cinemas/f7cd98e6-a6c5-4b1b-83dd-64fc9beccde8/timetables/15:00:00/places
     @GET("api/v1/cinemas/{idCine}/timetables/{time}/places")
     fun getSeatFromTime(
         @Path("idCine") idCine: String,
-        @Path("time") time: String ) : Call<List<seat>>
+        @Path("time") time: String
+    ): Call<List<seat>>
 
     @POST("api/v1/users/login")
-    fun Loguearse(@Body usuario: login) : Call<login>
+    fun Loguearse(@Body usuario: login): Call<login>
 
     @POST("api/v1/users")
-    fun Registrarse(@Body usuario: register) : Call<register>
+    fun Registrarse(@Body usuario: register): Call<register>
 
 }
