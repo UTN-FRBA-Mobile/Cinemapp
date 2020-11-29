03380f20ba213c2e7d932fe10b6d1ea2b9603c8a
Brian <brian.d.lobo@gmail.com>
2020-11-29 18:22:34 -0300

    funciona la vista de qe

--- app/src/main/java/com/utn/frba/cinemapp/presentation/ui/tickets/ListTicketAdapter.kt
+++ app/src/main/java/com/utn/frba/cinemapp/presentation/ui/tickets/ListTicketAdapter.kt
@@ -2,13 +2,16 @@ package com.utn.frba.cinemapp.presentation.ui.tickets
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import com.utn.frba.cinemapp.R
+import com.utn.frba.cinemapp.config.URL_PROXY_IMAGES
 import com.utn.frba.cinemapp.domain.entities.tickets.TicketOutEntity
 import kotlinx.android.synthetic.main.activity_tickets_item.view.*
 import java.text.SimpleDateFormat
@@ -51,14 +54,14 @@ class ViewHolderTickets(
         val sdf = SimpleDateFormat("dd/MM/yyyy")
         vista.title.text = sdf.format(ticket.purchaseDate)
 
-//        val finalUrl = URL_PROXY_IMAGES + ticket.posterPath?.replace("/", "")
-//        Picasso.get().load(finalUrl).into(vista.movie_image)
+        val finalUrl = URL_PROXY_IMAGES + ticket.movie.posterId.replace("/", "")
+        Picasso.get().load(finalUrl).into(vista.ticket_image)
 
-//        vista.popular_movies_constraint_layout.setOnClickListener {
-//
-//            val detailIntent = Intent(context, DetailMovieActivity::class.java)
-//            detailIntent.putExtra(DetailMovieActivity.KEY_MOVIE_SELECTED_ID, ticket.id)
-//            context.startActivity(detailIntent)
-//        }
+        vista.tickets_constraint_layout.setOnClickListener {
+
+            val detailIntent = Intent(context, TicketQrActivity::class.java)
+            detailIntent.putExtra(TicketQrActivity.KEY_TICKET_SELECTED_ID, ticket.qr.id)
+            context.startActivity(detailIntent)
+        }
     }
 }
