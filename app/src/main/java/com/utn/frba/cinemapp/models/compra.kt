bf982a79a08171a0156f5e9b788c113f3e328178
e-xifre <exifre@est.frba.utn.edu.ar>
2020-11-29 22:03:20 -0300

    se agrega el descuento

--- app/src/main/java/com/utn/frba/cinemapp/models/compra.kt
+++ app/src/main/java/com/utn/frba/cinemapp/models/compra.kt
@@ -2,11 +2,14 @@ package com.utn.frba.cinemapp.models
 
 import java.io.Serializable
 
-data class compra(var idCine: String = "",
+data class compra(var idCine: String? = "",
                   var horario: String? = "",
-                  var listaAsientos: List<String> = emptyList(),
-                  var numeroTarjeta: String = "",
-                  var idUsuario: String = "",
-                  var email: String = "",
-                  var idPelicula: Int = 0
+                  var listaAsientos: List<Int>? = emptyList(),
+                  var numeroTarjeta: String? = "",
+                  var idUsuario: String? = "",
+                  var email: String? = "",
+                  var idPelicula: Int? = 0,
+                  var dia: String? = "",
+                  var hora: String? = "",
+                  var descuento: String?
 ): Serializable
